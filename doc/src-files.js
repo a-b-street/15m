7518e4fee8659a4e3fc63e10464422e2ab20caab
dabreegster <dabreegster@users.noreply.github.com>
2025-09-03 13:53:05 +0000

    deploy: 6cfd7b216aa06d281e8b02485f9e450075961623

--- doc/src-files.js
+++ doc/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["graph",["",[["gtfs",[],["ids.rs","mod.rs","scrape.rs"]]],["create.rs","isochrone.rs","lib.rs","route.rs","snap.rs","timer.rs","transit_route.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[149]}
+createSrcSidebar('[["graph",["",[["gtfs",[],["ids.rs","mod.rs","scrape.rs"]]],["create.rs","isochrone.rs","lib.rs","route.rs","snap.rs","timer.rs","transit_route.rs"]]]]');
+//{"start":19,"fragment_lengths":[149]}
