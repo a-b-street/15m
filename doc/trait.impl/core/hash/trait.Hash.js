ea685dd5a4aeedbb70ae3eab79a3c29a2aa59f56
dabreegster <dabreegster@users.noreply.github.com>
2026-05-29 10:03:18 +0000

    deploy: 818452af9edb7a05384261543b19a52789f5c319

--- doc/trait.impl/core/hash/trait.Hash.js
+++ doc/trait.impl/core/hash/trait.Hash.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["graph",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"graph/struct.IntersectionID.html\" title=\"struct graph::IntersectionID\">IntersectionID</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"graph/struct.ProfileID.html\" title=\"struct graph::ProfileID\">ProfileID</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"graph/struct.RoadID.html\" title=\"struct graph::RoadID\">RoadID</a>"]]]]);
+    const implementors = Object.fromEntries([["graph",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"graph/struct.IntersectionID.html\" title=\"struct graph::IntersectionID\">IntersectionID</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"graph/struct.ProfileID.html\" title=\"struct graph::ProfileID\">ProfileID</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"graph/struct.RoadID.html\" title=\"struct graph::RoadID\">RoadID</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[758]}
+//{"start":59,"fragment_lengths":[764]}
